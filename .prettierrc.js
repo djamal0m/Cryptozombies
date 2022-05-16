9c87f22af42cea1fe523de8f2946adfa13935ffa
Djamal Moustapha <43470092+djameuzz@users.noreply.github.com>
2022-05-16 03:46:42 +0200

    project configuration

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    printWidth : 80,
+    tabWidth: 2,
+    useTabs : true,
+    semi: true,
+    trailingComa : "all",
+    bracketSpacing : true,
+    jsxBraketSameLine  : false,
+    arrowParents: "always",
+    proseWrap: "never",
+    htmlWhitespaceSensitivity: strict,
+    endOfLine : "lf",
+
+};
